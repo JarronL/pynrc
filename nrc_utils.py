fa0a80e5bb43bfbb6d975c9e7e7a5de9403caefa
Jarron Leisenring <jarronl@gmail.com>
2017-02-24 10:38:35 -0700

    comment lazy_thunkify
    This function isn’t used anywhere.
    Also “except Exception, e” isn’t compatible with Python 3. Instead,
    write “except Exception as e”

--- nrc_utils.py
+++ nrc_utils.py
@@ -2825,75 +2825,75 @@ def nrc_header(det_class, filter=None, pupil=None, obs_time=None, header=None):
 
 
 
-def lazy_thunkify(f):
-	"""
-	Make a function immediately return a function of no args which, when called,
-	waits for the result, which will start being processed in another thread.
-
-	This decorator will cause any function to, instead of running its code, 
-	start a thread to run the code, returning a thunk (function with no args) that 
-	waits for the function's completion and returns the value (or raises the exception).
-
-	Useful if you have Computation A that takes x seconds and then uses Computation B, 
-	which takes y seconds. Instead of x+y seconds you only need max(x,y) seconds.
-	
-	Example:
-
-	@lazy_thunkify
-	def slow_double(i):
-		print "Multiplying..."
-		time.sleep(5)
-		print "Done multiplying!"
-		return i*2
-
-	def maybe_multiply(x):
-		double_thunk = slow_double(x)
-		print "Thinking..."
-		time.sleep(3)
-		time.sleep(3)
-		time.sleep(1)
-		if x == 3:
-			print "Using it!"
-			res = double_thunk()
-		else:
-			print "Not using it."
-			res = None
-		return res
-
-	#both take 7 seconds
-	maybe_multiply(10)
-	maybe_multiply(3)
-
-	"""
-
-	@functools.wraps(f)
-	def lazy_thunked(*args, **kwargs):
-		wait_event = threading.Event()
-
-		result = [None]
-		exc = [False, None]
-
-		def worker_func():
-			try:
-				func_result = f(*args, **kwargs)
-				result[0] = func_result
-			except Exception, e:
-				exc[0] = True
-				exc[1] = sys.exc_info()
-				print("Lazy thunk has thrown an exception (will be raised on thunk()):\n%s" % 
-					(traceback.format_exc()))
-			finally:
-				wait_event.set()
-
-		def thunk():
-			wait_event.wait()
-			if exc[0]:
-				raise exc[1][0], exc[1][1], exc[1][2]
-
-			return result[0]
-
-		threading.Thread(target=worker_func).start()
-
-		return thunk
-
-	return lazy_thunked
+def lazy_thunkif# y(f):
+# 	"""
+# 	Make a function immediately return a function of no args which, when called,
+# 	waits for the result, which will start being processed in another thread.
+# 
+# 	This decorator will cause any function to, instead of running its code, 
+# 	start a thread to run the code, returning a thunk (function with no args) that 
+# 	waits for the function's completion and returns the value (or raises the exception).
+# 
+# 	Useful if you have Computation A that takes x seconds and then uses Computation B, 
+# 	which takes y seconds. Instead of x+y seconds you only need max(x,y) seconds.
+# 	
+# 	Example:
+# 
+# 	@lazy_thunkify
+# 	def slow_double(i):
+# 		print "Multiplying..."
+# 		time.sleep(5)
+# 		print "Done multiplying!"
+# 		return i*2
+# 
+# 	def maybe_multiply(x):
+# 		double_thunk = slow_double(x)
+# 		print "Thinking..."
+# 		time.sleep(3)
+# 		time.sleep(3)
+# 		time.sleep(1)
+# 		if x == 3:
+# 			print "Using it!"
+# 			res = double_thunk()
+# 		else:
+# 			print "Not using it."
+# 			res = None
+# 		return res
+# 
+# 	#both take 7 seconds
+# 	maybe_multiply(10)
+# 	maybe_multiply(3)
+# 
+# 	"""
+# 
+# 	@functools.wraps(f)
+# 	def lazy_thunked(*args, **kwargs):
+# 		wait_event = threading.Event()
+# 
+# 		result = [None]
+# 		exc = [False, None]
+# 
+# 		def worker_func():
+# 			try:
+# 				func_result = f(*args, **kwargs)
+# 				result[0] = func_result
+# 			except Exception as e:
+# 				exc[0] = True
+# 				exc[1] = sys.exc_info()
+# 				print("Lazy thunk has thrown an exception (will be raised on thunk()):\n%s" % 
+# 					(traceback.format_exc()))
+# 			finally:
+# 				wait_event.set()
+# 
+# 		def thunk():
+# 			wait_event.wait()
+# 			if exc[0]:
+# 				raise exc[1][0], exc[1][1], exc[1][2]
+# 
+# 			return result[0]
+# 
+# 		threading.Thread(target=worker_func).start()
+# 
+# 		return thunk
+# 
+# 	return lazy_thunked
