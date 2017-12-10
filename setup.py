4d3ea23599c62b0942821d3d47b430d4862a3c03
Jarron Leisenring <jarronl@gmail.com>
2017-12-09 19:31:34 -0700

    setup.py updates

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from pynrc.version import __version__
 version = __version__
 
 # Get the long description from the README file
-with open(path.join(root, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(root, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
