646b16d32909311af6c8937e82849f779d194a06
Jarron Leisenring <JarronL@users.noreply.github.com>
2021-12-22 00:05:45 -0700

    webbpsf_ext version 1.0.2 requirement

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ requirements = [
     'pysynphot>=2.0.0',
     'poppy>=1.0.1',
     'webbpsf>=1.0.0',
-    'webbpsf_ext>=1.0.0',
+    'webbpsf_ext>=1.0.2',
     'tqdm>4',
     'astropy>=4.2',
     'astroquery>=0.4.3',
