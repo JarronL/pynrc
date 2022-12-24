89017b01350ec30ab3946f1d4f58878a881e105c
Jarron Leisenring <JarronL@users.noreply.github.com>
2022-12-24 13:43:02 -0700

    Remove deprecated astropy-helpers module

--- setup.py
+++ setup.py
@@ -13,19 +13,7 @@ import sys
 import importlib #imp
 import ast
 
-################################################################
-# astropy-helpers
-################################################################
-
-import ah_bootstrap
-#A dirty hack to get around some early import/configurations ambiguities
-import builtins
-builtins._ASTROPY_SETUP_ = True
-
-from astropy_helpers.setup_helpers import (
-    register_commands, get_debug_option, get_package_info)
-from astropy_helpers.git_helpers import get_git_devstr
-from astropy_helpers.version_helpers import generate_version_py
+from git_helpers import get_git_devstr
 
 ################################################################
 # boiler plate
